a64f740cff216f797f8c82ab3a1b3b8e2aad5455
IfeanyiPlayzz <isiguzoflorence521@gmail.com>
2026-03-27 18:34:11 -0400

    Delete src directory

--- src/hw/memory.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEMORY_H
-#define MEMORY_H
-#include <stdint.h>
-
-uint32_t mem_read32(uint32_t addr);
-void mem_write32(uint32_t addr, uint32_t val);
-
-#endif
--- src/rcp/rsp.c
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-// The RSP handles geometry and audio via Microcode (ucode)
-void rsp_step() {
-    // Fetch RSP instructions from IMEM
-    // Execute vector operations (SIMD)
-}
